fc4aac69202803a1b18c12f581cc151712310190
James H. Hill <hilljh82@gmail.com>
2017-07-11 21:12:00 -0400

    Merged determinate and indeterminate progress bar components

--- addon/components/materialize-progress-indeterminate.js
+++
@@ -1,6 +0,0 @@
-import MaterializeProgress from './materialize-progress';
-import layout from '../templates/components/materialize-progress-indeterminate';
-
-export default MaterializeProgress.extend({
-  layout
-});
--- addon/components/materialize-progress.js
+++ addon/components/materialize-progress.js
@@ -3,5 +3,23 @@ import layout from '../templates/components/materialize-progress';
 
 export default Ember.Component.extend({
   layout,
-  classNames: ['progress']
+
+  classNames: ['progress'],
+
+  classNameBindings: ['isDeterminate:determinate:indeterminate'],
+
+  mode: 'indeterminate',
+
+  didUpdateAttrs (attrs) {
+    this._super (...arguments);
+
+    if (this.get ('style') === 'determinate') {
+      if (Ember.isPresent (attrs.newAttrs.progress)) {
+        Ember.run ('afterRender', () => {
+          let progress = attrs.newAttrs.progress.value * 100;
+          this.$('.determinate').css ('width', `${progress}%`);
+        });
+      }
+    }
+  }
 });
