747e0b70142fc77a3da491c0e34d59377c19d86a
Evan Wilt <ev.wilt@gmail.com>
2017-08-02 16:38:09 -0400

    Removed unnecessarty if statement

--- addon/components/materialize-carousel.js
+++ addon/components/materialize-carousel.js
@@ -5,25 +5,20 @@ export default Ember.Component.extend({
   layout,
   classNames: ['carousel'],
 
-  didRender (attrs) {
+  didRender () {
     this._super (...arguments);
 
-    if (Ember.isPresent (attrs.newAttrs.fullWidth) || Ember.isPresent (attrs.newAttrs.duration) ||
-      Ember.isPresent (attrs.newAttrs.dist) || Ember.isPresent (attrs.newAttrs.shift) ||
-      Ember.isPresent (attrs.newAttrs.padding) || Ember.isPresent (attrs.newAttrs.indicators) ||
-      Ember.isPresent (attrs.newAttrs.noWrap)) {
-      let opts = {
-        fullWidth: this.get ('fullWidth'),
-        duration: this.get ('durations'),
-        dist: this.get ('dist'),
-        shift: this.get ('shift'),
-        padding: this.get ('padding'),
-        indicators: this.get ('indicators'),
-        noWrap: this.get ('noWrap')
-      };
-      let $this = this.$ ();
-      $this.carousel (opts);
-    }
+    let opts = {
+      fullWidth: this.get ('fullWidth'),
+      duration: this.get ('durations'),
+      dist: this.get ('dist'),
+      shift: this.get ('shift'),
+      padding: this.get ('padding'),
+      indicators: this.get ('indicators'),
+      noWrap: this.get ('noWrap')
+    };
+    let $this = this.$ ();
+    $this.carousel (opts);
   }
 });
 
