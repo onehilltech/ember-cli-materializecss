f0183e2cb584abd190d29e1eee233b41d7371441
James H. Hill <hilljh82@gmail.com>
2017-08-03 01:43:29 -0400

    Updated add-on blueprint to create _app-materialize.scss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,7 @@ let EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    sassOptions: {
-      includePaths: [
-        'bower_components/materialize/sass'
-      ]
-    }
+
   });
 
   /*
@@ -17,22 +13,6 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-
-  app.import ({
-    development: 'bower_components/materialize/dist/js/materialize.js',
-    production: 'bower_components/materialize/dist/js/materialize.min.js'
-  });
-
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Bold.woff', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Bold.woff2', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Light.woff', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Light.woff2', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Medium.woff', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Medium.woff2', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Regular.woff', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Regular.woff2', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Thin.woff', {destDir: 'fonts/roboto'});
-  app.import ('bower_components/materialize/fonts/roboto/Roboto-Thin.woff2', {destDir: 'fonts/roboto'});
 
   return app.toTree();
 };
