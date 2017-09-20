ef56554b42c6218f86b9a211c1f577a13f21819f
James H. Hill <hilljh82@gmail.com>
2017-09-20 08:07:24 -0400

    Fixed export statement

--- addon/index.js
+++ addon/index.js
@@ -1,2 +1,3 @@
-export {InputFieldMixin} from './-private/mixins/input-field';
-export {TextInputFieldMixin} from './-private/mixins/text-input-field';
+export {default as InputFieldMixin} from './-private/mixins/input-field';
+export {default as TextInputFieldMixin} from './-private/mixins/text-input-field';
+export {default as TextField} from './components/materialize-textfield';
