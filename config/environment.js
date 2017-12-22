f6fbcfb529a8dd4a16f66c6fe1a479389cf63d0a
James H. Hill <hilljh82@gmail.com>
2017-12-22 08:56:22 -0500

    Environment did not support multiple add-ons configuring sassOptions

--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,17 @@
 /*jshint node:true*/
 'use strict';
 
-module.exports = function (/*environment, appConfig*/) {
-  return {
-    sassOptions: {
-      includePaths: [
-        'bower_components/materialize/sass',
-        'app/styles'
-      ]
-    }
-  };
+module.exports = function (environment, appConfig) {
+  if (!appConfig.sassOptions)
+    appConfig.sassOptions = {};
+
+  if (!appConfig.sassOptions.includePaths)
+    appConfig.sassOptions.includePaths = [];
+
+  appConfig.sassOptions.includePaths = appConfig.sassOptions.includePaths.concat ([
+    'bower_components/materialize/sass',
+    'app/styles'
+  ]);
+
+  return appConfig;
 };
